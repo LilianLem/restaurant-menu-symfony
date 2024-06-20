640f3f1b254e8a78207a1139708701b7d466f503
LilianLem <54894780+LilianLem@users.noreply.github.com>
2024-06-20 19:40:34 +0200

    Small fix on an interface
    Removing a method recently transferred in a service

--- src/Entity/Interface/SoftDeleteableEntityInterface.php
+++ src/Entity/Interface/SoftDeleteableEntityInterface.php
@@ -16,8 +16,6 @@ interface SoftDeleteableEntityInterface
     /** Check if the entity has been soft deleted */
     public function isDeleted(): bool;
 
-    public function softDelete(bool $handleParents = false): self;
-
     /** @return Collection<int, self>|self|null */
     public function getChildren(): Collection|self|null;
 
